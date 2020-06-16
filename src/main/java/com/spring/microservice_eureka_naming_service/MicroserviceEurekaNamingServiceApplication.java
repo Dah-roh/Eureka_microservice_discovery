a4553a5624ca227098bd7edb5cf0bd50e7a2de5e
dizsart <darogadibia@gmail.com>
2020-06-16 14:47:37 +0100

    add intial commit

---
+++ src/main/java/com/spring/microservice_eureka_naming_service/MicroserviceEurekaNamingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.microservice_eureka_naming_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceEurekaNamingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceEurekaNamingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/microservice_eureka_naming_service/MicroserviceEurekaNamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.microservice_eureka_naming_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEurekaNamingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
